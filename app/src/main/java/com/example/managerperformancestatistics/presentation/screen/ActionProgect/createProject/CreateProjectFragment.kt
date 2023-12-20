dd035feea1e8a1d8f1d712a01051bf650a8d2c41
Kristina Dimitrieva <98698205+FroschMadchen@users.noreply.github.com>
2023-12-20 20:27:49 +0300

    create action image

--- app/src/main/java/com/example/managerperformancestatistics/presentation/screen/ActionProgect/createProject/CreateProjectFragment.kt
+++ app/src/main/java/com/example/managerperformancestatistics/presentation/screen/ActionProgect/createProject/CreateProjectFragment.kt
@@ -1,17 +1,23 @@
 package com.example.managerperformancestatistics.presentation.screen.ActionProgect.createProject
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import com.example.managerperformancestatistics.R
 import com.example.managerperformancestatistics.databinding.FragmentCreateProjectBinding
 
 
 class CreateProjectFragment : Fragment() {
+    private val PICK_IMAGE = 4
+
     private var _binding: FragmentCreateProjectBinding? = null
     private lateinit var mController: NavController
     override fun onCreateView(
@@ -28,5 +34,62 @@ class CreateProjectFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         mController = findNavController()
 
+        _binding?.apply {
+            addImage.setOnClickListener {
+                val getIntent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+                getIntent.type = "image/*"
+                getIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+                val pickIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                pickIntent.type = "image/*"
+
+                val chooserIntent = Intent.createChooser(getIntent, "Select Image")
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(pickIntent))
+
+                startActivityForResult(chooserIntent, PICK_IMAGE)
+
+            }
+        }
+
+
     }
-}
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Unit {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            // Обработка результата выбора изображения
+
+            val images: MutableList<Uri> = mutableListOf()
+
+            data?.clipData?.let { clipData ->
+                for (i in 0 until clipData.itemCount) {
+                    val uri = clipData.getItemAt(i).uri
+                    images.add(uri)
+                }
+
+                Log.i("ImageCollection","Number of images: ${images.size}")
+                images.forEachIndexed { index, uri ->
+                    Log.i("Image","Image $index: $uri")
+                }
+            }
+         /*   val selectedImageUri = data?.data
+            if (selectedImageUri != null) {*/
+                _binding?.apply {
+                  /*  imageView3.setImageURI(selectedImageUri)
+                    imageView3.setVisibility(View.VISIBLE)*/
+
+                        imageView3.setImageURI(images[0])
+                        //  imageView3.visibility = View.VISIBLE
+                        Log.i("Image","Image ${images[0]}")
+                    image1.setImageURI(images[1])
+                    Log.i("Image","Image ${images[1]}")
+                    image2.setImageURI(images[2])
+                    image3.setImageURI(images[3])
+                    }
+
+                }
+            }
+        }
+
+
